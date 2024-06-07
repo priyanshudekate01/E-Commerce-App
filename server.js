27578e1ae8eb1308d504269b5555131ce78eac5f
Priyanhsu Dekate <priyanshudekate@gmail.com>
2024-06-07 23:28:17 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+// adding type : module in package json to use express like a react
+import express from "express";
+import dotenv from "dotenv";
+
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+
+//configure env
+dotenv.config();
+
+//database config
+connectDB();
+
+const app = express();
+
+//Middleware
+app.use(express.json());
+app.use(morgan("dev"));
+
+//routes
+app.use("/api/v1/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("<h1>hello world</h1>");
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
