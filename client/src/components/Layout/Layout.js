5bc32dfbe49a5b473010cb58c33b95e04ddd7fb6
Priyanhsu Dekate <priyanshudekate@gmail.com>
2024-06-15 22:53:54 +0530

    adding alert msg, concurrently running both frontend and backend in same terminal

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import { Helmet } from "react-helmet";
+import { ToastContainer } from "react-toastify";
+
 const Layout = ({ children, title, description, keywords, author }) => {
   return (
     <div>
@@ -14,7 +16,10 @@ const Layout = ({ children, title, description, keywords, author }) => {
       </Helmet>
       <Header />
 
-      <main style={{ minHeight: "70vh" }}>{children}</main>
+      <main style={{ minHeight: "70vh" }}>
+        <ToastContainer />
+        {children}
+      </main>
       <Footer />
     </div>
   );
